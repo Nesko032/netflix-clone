94dab870b3cf5eef2ea8ac37fbbb73a0a570604d
Hữu Phúc <20521770@gm.uit.edu.vn>
2023-11-07 16:46:44 +0700

    Sign in & Sign Up

---
+++ pages/auth.tsx
@@ -0,0 +1,89 @@
+import { useCallback, useState } from "react";
+import Input from "@/components/input";
+
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    // switch login and create account
+    const [variant, setVariant] = useState("signIn");
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) =>
+            currentVariant === "signIn" ? "signUp" : "signIn"
+        );
+    }, []);
+
+    return (
+        <div className="h-full w-full relative bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-center bg-cover">
+            <div className="h-full w-full bg-black lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img
+                        src="/images/logo.png"
+                        alt="Logo Netflix"
+                        className="h-12"
+                    />
+                </nav>
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold">
+                            {variant === "signIn" ? "Sign in" : "Sign up"}
+                        </h2>
+
+                        <div className="flex flex-col gap-4">
+                            {variant === "signUp" && (
+                                <Input
+                                    id="name"
+                                    type="text"
+                                    label="Username"
+                                    value={name}
+                                    onChange={(e: any) =>
+                                        setName(e.target.value)
+                                    }
+                                />
+                            )}
+                            <Input
+                                id="email"
+                                type="email"
+                                label="Email"
+                                value={email}
+                                onChange={(e: any) => setEmail(e.target.value)}
+                            />
+
+                            <Input
+                                id="password"
+                                type="password"
+                                label="Password"
+                                value={password}
+                                onChange={(e: any) =>
+                                    setPassword(e.target.value)
+                                }
+                            />
+                        </div>
+
+                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            {variant === "signIn" ? "Log in" : "Register"}
+                        </button>
+
+                        <p className="text-neutral-500 mt-12">
+                            {variant === "signIn"
+                                ? "First time using Netflix?"
+                                : "Already have an account?"}
+                            <span
+                                className="text-white ml-1 hover:underline cursor-pointer"
+                                onClick={toggleVariant}
+                            >
+                                {variant === "signIn"
+                                    ? "Create an account"
+                                    : "Login with account"}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Auth;
